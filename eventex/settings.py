e0af83bbceee1f4e29f3ed556aeda5200828ef2f
Alfredo Stanquini Neto <stanquini@gmail.com>
2019-11-17 21:01:26 -0300

    ajuste

--- eventex/settings.py
+++ eventex/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from decouple import config, Csv
+from decouple import config
 from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -22,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config(‘SECRET_KEY’)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config(‘DEBUG’, default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [’*’]
 
 
 # Application definition
@@ -76,9 +76,9 @@ WSGI_APPLICATION = 'eventex.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+default_dburl = ‘sqlite:///’ + os.path.join(BASE_DIR, ‘db.sqlite3’)
 DATABASES = {
-    'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
+    ‘default’: config(‘DATABASE_URL’, default=default_dburl, cast=dburl),
 }
 
 
@@ -119,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, ‘staticfiles’)
--- eventex/wsgi.py
+++ eventex/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eventex.settings')
+os.environ.setdefault(“DJANGO_SETTINGS_MODULE”, “eventex.settings”)
 
 application = Cling(get_wsgi_application())
