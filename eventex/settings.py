469daa775251a38b5a953d6ee3d4dab02a6667b8
Alfredo Stanquini Neto <stanquini@gmail.com>
2019-11-25 23:17:49 -0300

    ajuste variavel DATABASES

--- eventex/settings.py
+++ eventex/settings.py
@@ -74,9 +74,7 @@ WSGI_APPLICATION = 'eventex.wsgi.application'
 
 default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 DATABASES = {
-    'default': {
         'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
-    }
 }
 
 
