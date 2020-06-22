061cec40396773903b0a4bce8f5fafb759df0202
Alfredo Stanquini Neto <stanquini@gmail.com>
2020-06-21 21:23:39 -0300

    Contatos

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.shortcuts import resolve_url as r
 
+
 class Speaker(models.Model):
     name = models.CharField('nome', max_length=255)
     slug = models.SlugField('slug')
@@ -17,3 +18,23 @@ class Speaker(models.Model):
 
     def get_absolute_url(self):
         return r('speaker_detail', slug=self.slug)
+
+
+class Contact(models.Model):
+    EMAIL = 'E'
+    PHONE = 'P'
+
+    KINDS = (
+        (EMAIL, 'Email'),
+        (PHONE, 'Telefone'),
+    )
+    speaker = models.ForeignKey('Speaker', on_delete=models.CASCADE, verbose_name='palestrante')
+    kind = models.CharField('tipo', max_length=1, choices=KINDS)
+    value = models.CharField('valor',max_length=255)
+
+    class Meta:
+        verbose_name = 'contato'
+        verbose_name_plural = 'contatos'
+
+    def __str__(self):
+        return self.value
