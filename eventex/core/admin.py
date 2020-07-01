42ce6738c359c0541c779deeb9cafb9bfb2e6c50
Alfredo Stanquini Neto <stanquini@gmail.com>
2020-06-30 22:49:14 -0300

    Course como Abstract Base Class

--- eventex/core/admin.py
+++ eventex/core/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.html import format_html
 
-from eventex.core.models import Speaker, Contact, Talk
+from eventex.core.models import Speaker, Contact, Talk, Course
 
 
 class ContactInline(admin.TabularInline):
@@ -38,3 +38,4 @@ class SpeakerModelAdmin(admin.ModelAdmin):
 
 admin.site.register(Speaker, SpeakerModelAdmin)
 admin.site.register(Talk)
+admin.site.register(Course)
